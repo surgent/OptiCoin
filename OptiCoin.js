a547424938655ade55d7066a3844d10a17dca943
Bill <surgent@eden.rutgers.edu>
2014-11-09 22:18:25 -0500

    Contrast, Luminance Threshold, and Convolution Kernels for Box Blur, Gaussian Blur, Sharpness, and Edge filtering.

--- OptiCoin.js
+++ OptiCoin.js
@@ -1,5 +1,36 @@
 var OptiCoin = {};
 
+OptiCoin.contrast = function(data, amount) {
+	var d = data.data;
+	var l = d.length;
+	for(var i = 0; i < l; ++i) {
+		if((i & 3) == 3) continue;
+		
+		var v = (d[i] - 128) * amount + 128;
+		v = ~~v;
+		if(v < 0) v = 0;
+		if(v > 255) v = 255;
+		d[i] = v;
+	}
+}
+
+OptiCoin.threshold = function(data, lum) {
+	var d = data.data;
+	var l = d.length;
+	for(var i = 0; i < l; i += 4) {
+		if(d[i] * d[i] + d[i + 1] * d[i + 1] + d[i + 2] * d[i + 2] > lum * lum) {
+			d[i] = 255;
+			d[i + 1] = 255;
+			d[i + 2] = 255;
+		}
+		else {
+			d[i] = 0;
+			d[i + 1] = 0;
+			d[i + 2] = 0;
+		}
+	}
+}
+
 OptiCoin.vectorThreshold = function(data, r, g, b, dotTol, magTol) {
 	var dat = data.data;
 	var len = dat.length;
@@ -29,6 +60,57 @@ OptiCoin.vectorThreshold = function(data, r, g, b, dotTol, magTol) {
 			dat[i + 1] = 255;
 			dat[i + 2] = 255;
 		}
+	}
+}
+
+OptiCoin.Kernel = {};
+OptiCoin.Kernel.BOX_BLUR = [1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9, 1/9];
+OptiCoin.Kernel.GAUSSIAN_BLUR = [1/16, 1/8, 1/16, 1/8, 1/4, 1/8, 1/16, 1/8, 1/16];
+OptiCoin.Kernel.SHARP = [0, -1, 0, -1, 5, -1, 0, -1, 0];
+OptiCoin.Kernel.EDGE = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
+
+OptiCoin.convolve = function(data, kernel) {
+	var w = data.width;
+	var h = data.height;
+	var dat = data.data;
+	var k = kernel;
+	var prevRow = null;
+	var prevPix = null;
+
+	for(var y = 0; y < h; ++y) {
+		prevPix = null;
+		var prevRowTemp = Array.prototype.slice.call(dat, y * w * 4, (y + 1) * w * 4);
+
+		for(var x = 0; x < w; ++x) {
+			var prevPixTemp = Array.prototype.slice.call(dat, y * w * 4 + x * 4, y * w * 4 + (x + 1) * 4);
+			var m11, m12, m13, m21, m22, m23, m31, m32, m33;
+
+			for(var i = 0; i < 3; ++i) {
+				m11 = x == 0 ? (y == 0 ? dat[i] : prevRow[i]) : (y == 0 ? prevPix[i] : prevRow[(x - 1) * 4 + i]);
+				m12 = y == 0 ? dat[x * 4 + i] : prevRow[x * 4 + i];
+				m13 = x == w - 1 ? (y == 0 ? dat[x * 4 + i] : prevRow[x * 4 + i]) : (y == 0 ? dat[(x + 1) * 4 + i] : prevRow[(x + 1) * 4 + i]);
+				m21 = x == 0 ? dat[y * w * 4 + x * 4 + i] : prevPix[i];
+				m22 = dat[y * w * 4 + x * 4 + i];
+				m23 = x == w - 1 ? dat[y * w * 4 + x * 4 + i] : dat[y * w * 4 + (x + 1) * 4 + i];
+				m31 = x == 0 ? (y == h - 1 ? dat[y * w * 4 + x * 4 + i] : dat[(y + 1) * w * 4 + x * 4 + i]) : (y == h - 1 ? dat[y * w * 4 + x * 4 + i] : dat[(y + 1) * w * 4 + (x - 1) * 4 + i]);
+				m32 = y == h - 1 ? dat[y * w * 4 + i] : dat[(y + 1) * w * 4 + x * 4 + i];
+				m33 = x == w - 1 ? (y == h - 1 ? dat[y * w * 4 + x * 4 + i] : dat[(y + 1) * w * 4 + x * 4 + i]) : (y == h - 1 ? dat[y * w * 4 + (x + 1) * 4 + i] : dat[(y + 1) * w * 4 + (x + 1) * 4 + i]);
+			
+				var val = k[0] * m11 + k[1] * m12 + k[2] * m13 +
+				          k[3] * m21 + k[4] * m22 + k[5] * m23 +
+				          k[6] * m31 + k[7] * m32 + k[8] * m33;
+
+				val = ~~val;
+				if(val < 0) val = 0;
+				if(val > 255) val = 255;
+
+				dat[y * w * 4 + x * 4 + i] = val;
+			}
+
+			prevPix = prevPixTemp;
+		}
+
+		prevRow = prevRowTemp;
 	}
 }
 
